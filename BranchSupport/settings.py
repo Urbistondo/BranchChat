36567dc0173045e3d3f2a5db45572a87d6e0a173
Javier Urbistondo <urbistondo.javier@gmail.com>
2020-01-04 09:48:12 -0800

    Adds reconnecting websocket in the client

--- BranchSupport/settings.py
+++ BranchSupport/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'channels',
+    'crispy_forms',
     'rest_framework',
     'rest_framework_nested',
 
@@ -134,5 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'BranchSupport/static')
+]
 
 APPEND_SLASH = False
